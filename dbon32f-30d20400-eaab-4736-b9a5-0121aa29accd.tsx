245320759c0ede3fd0131fdcf3b37a7e14408fe4
antoine <antoine@nc-ass-vip.sdv.fr>
2021-02-04 14:04:32 +0100

    Map

---
+++ dbon32f-30d20400-eaab-4736-b9a5-0121aa29accd.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="dbon32f-30d20400-eaab-4736-b9a5-0121aa29accd" tilewidth="32" tileheight="32" tilecount="90" columns="9">
+ <image source="dbon32f-30d20400-eaab-4736-b9a5-0121aa29accd.png" width="316" height="350"/>
+</tileset>
---
+++ ddpy4ut-2bdac208-fbdc-4ce0-81e1-090763f94e50.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="ddpy4ut-2bdac208-fbdc-4ce0-81e1-090763f94e50" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="ddpy4ut-2bdac208-fbdc-4ce0-81e1-090763f94e50.png" width="256" height="384"/>
+</tileset>
