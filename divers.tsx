38037f0952e7c84a0c19f5c99bffac6167c2f768
antoine <antoine@nc-ass-vip.sdv.fr>
2021-04-02 08:55:11 +0200

    N

---
+++ divers.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="divers" tilewidth="32" tileheight="32" tilecount="1744" columns="8">
+ <image source="divers.png" width="256" height="7000"/>
+</tileset>
